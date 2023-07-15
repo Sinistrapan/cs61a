2c08fb859686358efbcb6c1611264865b3368f82
Sinistrapan <sinistrapan@gmail.com>
2023-07-15 07:15:24 -0700

    new file:   hw09/.ok_history 	new file:   hw09/.ok_storage 	modified:   hw09/hw09.scm 	new file:   hw09/tests/__pycache__/__init__.cpython-310.pyc 	new file:   hw09/tests/__pycache__/ascending.cpython-310.pyc 	new file:   hw09/tests/__pycache__/filter.cpython-310.pyc 	new file:   hw09/tests/__pycache__/interleave.cpython-310.pyc 	new file:   hw09/tests/__pycache__/no_repeats.cpython-310.pyc 	modified:   hw09/tests/no_repeats.py

--- hw09/hw09.scm
+++ hw09/hw09.scm
@@ -1,7 +1,23 @@
-(define (ascending? asc-lst) 'YOUR-CODE-HERE)
+(define (ascending? asc-lst)
+  (if (or (null? asc-lst) (null? (cdr asc-lst)))
+      #t
+      (if (< (car asc-lst) (cadr asc-lst))
+          (ascending? (cdr asc-lst))
+          #f)))
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+(define (my-filter pred s)
+  (cond ((null? s) '())
+        ((pred (car s)) (cons (car s) (my-filter pred (cdr s))))
+        (else (my-filter pred (cdr s)))))
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2)
+  (if (null? lst1) lst2
+      (if (null? lst2) lst1
+          (cons (car lst1) (interleave lst2 (cdr lst1))))))
 
-(define (no-repeats lst) 'YOUR-CODE-HERE)
+(define (no-repeats lst)
+  (if (null? lst)
+      '()
+      (cons (car lst)
+            (no-repeats (my-filter (lambda (x) (not (= x (car lst)))) (cdr lst))))))
+
